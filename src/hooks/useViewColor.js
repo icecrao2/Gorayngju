cb5d93b9f3c16fae4dc86aa6ef9484d945663d25
jae woong <icecrao2@daum.net>
2022-11-08 17:28:02 +0900

    d

--- src/hooks/useViewColor.js
+++ src/hooks/useViewColor.js
@@ -13,6 +13,7 @@ export const useViewColor = () => {
         return () => {
             window.removeEventListener("resize", handleResize);
         }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [color]);
 
 
